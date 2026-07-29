037e88437c8c3a3646c0c4292fdfbc911b6d5da3
cere-code <sncrabram@gmail.com>
2026-07-29 02:18:53 -0400

    rearranged code and added comments

--- Follow_Filter.py
+++ Follow_Filter.py
@@ -7,6 +7,9 @@ with open("followers_1.json") as file:
 with open("following.json") as file:
     fingdata = json.load(file)
 
+# Creates a list, iterates through the json file,
+# adds formated rows into the list as nested dictionary's
+# turns list into dataframe
 fing_rows = []
 for relations in fingdata['relationships_following']:
     username = relations['title']
@@ -17,9 +20,7 @@ for relations in fingdata['relationships_following']:
 
     row = {'username': username, 'timestamp': timestamp, 'link': link}
     fing_rows.append(row)
-
 following_df = pd.DataFrame(fing_rows)
-# print(following_df)
 
 fer_rows = []
 for person in ferdata:
@@ -30,26 +31,15 @@ for person in ferdata:
 
     row2 = {'username': username, 'timestamp': timestamp, 'link': link}
     fer_rows.append(row2)
-
 follower_df = pd.DataFrame(fer_rows)
-# print(follower_df.to_string())
 
-found_non = following_df[
-    ~ following_df['username'].isin(follower_df['username'])
-]
+# compares dataframes
 found_fers = following_df[
-    following_df['username'].isin(follower_df['username'])
-]
+    following_df['username'].isin(follower_df['username'])]
+found_non = following_df[
+    ~ following_df['username'].isin(follower_df['username'])]
 
-
-def non_fers():
-    print(
-        "\n".join(
-            f"{r.username} | {r.link}\n - does not follow you back\n"
-            for r in found_non.itertuples()
-        )
-    )
-
+#prints code in selected format
 def fers():
     print(
         "\n".join(
@@ -57,7 +47,13 @@ def fers():
             for r in found_fers.itertuples()
         )
     )
-
+def non_fers():
+    print(
+        "\n".join(
+            f"{r.username} | {r.link}\n - does not follow you back\n"
+            for r in found_non.itertuples()
+        )
+    )
 def menu():
     select = {
         "1": ("Followers List", fers),
@@ -76,5 +72,6 @@ def menu():
     else:
         action = lambda: print("Invalid option")
     action()
+
 
 menu()
